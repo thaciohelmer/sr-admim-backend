37f18f290cb904337e61b285189ae914fdfd0880
Thacio Helmer <87128290+thaciohelmer@users.noreply.github.com>
2023-03-31 17:23:42 -0300

    Update Db Configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,29 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CategoriesModule } from './categories/categories.module';
 import { PlayersModule } from './players/players.module';
-
-const uri = 'mongodb://thaciohelmer:thaciohelmer123@localhost:27017'
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      uri,
+    MongooseModule.forRootAsync(
       {
-        dbName: 'sr_adm_backend'
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => {
+          const MG_USER = configService.get<string>('MG_USER')
+          const MG_PASSWORD = configService.get<string>('MG_PASSWORD')
+          const MG_URL = configService.get<string>('MG_URL')
+          const MG_DB_NAME = configService.get<string>('MG_DB_NAME')
+          const uri = `mongodb://${MG_USER}:${MG_PASSWORD}@${MG_URL}`
+          return {
+            uri,
+            dbName: MG_DB_NAME
+          }
+        }
       }
     ),
     CategoriesModule,
     PlayersModule,
+    ConfigModule.forRoot({ isGlobal: true })
   ],
   controllers: [],
   providers: []
